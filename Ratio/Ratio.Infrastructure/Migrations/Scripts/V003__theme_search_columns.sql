b6fa71b7b0fcff4c17e6db67be4c557021f0fd94
IsaacOliveiraSouza <isaacoliveirazuza28@gmail.com>
2026-09-22 20:34:39 -0300

    feat: add the full-text and trigram indexes to the theme dimension

--- Ratio/Ratio.Infrastructure/Migrations/Scripts/V003__theme_search_columns.sql
+++ Ratio/Ratio.Infrastructure/Migrations/Scripts/V003__theme_search_columns.sql
@@ -5,3 +5,7 @@ $$ SELECT lower(public.unaccent('public.unaccent'::regdictionary, txt)) $$;
 ALTER TABLE dw.dim_theme
     ADD COLUMN search_vector tsvector GENERATED ALWAYS AS (to_tsvector('dw.pt_unaccent'::regconfig, theme_name)) STORED,
     ADD COLUMN theme_name_norm text GENERATED ALWAYS AS (dw.norm_pt(theme_name)) STORED;
+
+CREATE INDEX idx_dim_theme_fts ON dw.dim_theme USING gin (search_vector);
+
+CREATE INDEX idx_dim_theme_name_trgm ON dw.dim_theme USING gin (theme_name_norm gin_trgm_ops);
